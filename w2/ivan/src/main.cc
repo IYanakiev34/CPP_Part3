5eb436fce5d20e2457b8b114d0f520d90768615e
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2023-02-21 15:40:46 +0100

    10 answers + demo

--- w2/ivan/src/main.cc
+++ w2/ivan/src/main.cc
@@ -46,30 +46,6 @@ int main(int argc, char **argv)
     */
 
     // 10
-    /*
-    Unique<int> value{42};
-    Unique<int> second{value};
-
-    std::cout << value.get() << " " << second.get() << "\n";
-    second.get() = 13;
-
-    std::cout << value.get() << " " << second.get() << "\n";
-
-    {
-        Unique<std::string> text{"hello world"};
-        text = Unique<std::string>{"hello world(2)"};
-
-        std::cout << text.get() << "\n";
-        std::cout << "Count strings: " << Unique<std::string>::s_count
-                  << ", Actual: " << Unique<std::string>::s_actual << "\n";
-    }
-    std::cout << "Count strings: " << Unique<std::string>::s_count
-              << ", Actual: " << Unique<std::string>::s_actual << "\n";
-
-    std::cout << "Count int: " << Unique<int>::s_count << ", Actual: "
-              << Unique<int>::s_actual << "\n";
-    */
-
     // 11
 
     Unique<int> value{42};
