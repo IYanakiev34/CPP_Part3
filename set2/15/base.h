b168b947939e88c1d92c0717305c85f4b0ccc03f
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2023-02-28 14:05:09 +0100

    15 base changed for submission

--- set2/15/base.h
+++ set2/15/base.h
@@ -3,20 +3,12 @@
 
 #include <iostream>
 
-template <typename Derived, typename Ret, typename... Args>
+template <typename Derived>
 class Base
 {
 public:
-    using FuncPtr = Ret (Derived::*)(Args...);
-    // Same for const funcptr
-
+    template <typename Ret, typename... Args>
     Ret nc(Args... args) { return static_cast<Derived *>(this)->nc_impl(args...); }
-};
-
-class Derived : public Base<Derived, int, double>
-{
-public:
-    int nc_impl(double d) { return static_cast<int>(d); }
 };
 
 #endif
